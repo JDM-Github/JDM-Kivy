43bf3323403926e41cacb69af7f22616d266ab68
JDMaster <numberonejdmaster@gmail.com>
2023-02-22 12:15:30 +0800

    Changes to be committed: 	modified:   src/jdm_kivy/all_color.json 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from src.jdm_kivy import *
 
+class MainWidget(JDMWidget): ...
+class MainScreen(JDMScreen): ...
+
 if __name__ == "__main__":
-    JDMApp().run()
+    JDMApp().run(screen_name="main", screen=MainScreen(), widget=MainWidget())
